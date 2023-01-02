9fed2cdbe6542876bbb4f979b7a1c09e2822b40a
Skulli73 <skulli730@gmail.com>
2023-01-02 16:42:54 +0100

    Somewhat fixed dm message stuff

--- src/main/java/io/github/skulli73/main/messageComponents/VoteMessageComponent.java
+++ src/main/java/io/github/skulli73/main/messageComponents/VoteMessageComponent.java
@@ -3,12 +3,20 @@ package io.github.skulli73.main.messageComponents;
 import io.github.skulli73.main.objects.Election;
 import io.github.skulli73.main.objects.Voter;
 import org.javacord.api.entity.channel.PrivateChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
-import org.javacord.api.entity.message.component.ActionRow;
-import org.javacord.api.entity.message.component.Button;
+import org.javacord.api.entity.message.MessageFlag;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.interaction.MessageComponentInteraction;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static io.github.skulli73.main.MainPsephos.*;
 
@@ -34,15 +42,37 @@ public class VoteMessageComponent {
 
         MessageBuilder lMessageBuilder = lElection.getBallot();
         try{
+            ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+
             PrivateChannel lDmChannel = lUser.openPrivateChannel().get();
-            lMessageBuilder.send(lDmChannel);
-            lElection.voters.add(new Voter(lUser.getId(), lElection.electoralMethod.amountOfComponents(lElection)));
-            elections.put(lElection.id, lElection);
-            saveElections();
+            CompletableFuture<Message> lFuture = lMessageBuilder.send(lDmChannel);
+            lFuture.thenAccept(c -> {
+                if (c != null) {
+                    lElection.voters.add(new Voter(lUser.getId(), lElection.electoralMethod.amountOfComponents(lElection)));
+                    elections.put(lElection.id, lElection);
+                    saveElections();
+                    pInteraction.acknowledge();
+                }
+            });
+
+            ScheduledFuture<?> scheduledFuture = executor.schedule(() -> {
+                if (!lFuture.isDone()) {
+                    pInteraction.createImmediateResponder().append("You have your dms disabled.").setFlags(MessageFlag.EPHEMERAL).respond();
+                }
+            }, 2, TimeUnit.SECONDS);
+
+            lFuture.exceptionally(t -> {
+                if (t instanceof IllegalStateException) {
+                    pInteraction.createImmediateResponder().append("You have your dms disabled.").setFlags(MessageFlag.EPHEMERAL).respond();
+                }
+                scheduledFuture.cancel(false);
+                return null;
+            });
+
+
         } catch(Exception e) {
+            System.err.println(e);
             return;
         }
-
-        pInteraction.acknowledge();
     }
 }
